8e3ab66003648098f6c25555d6db5571b24a465e
Isabelle <114663286+isaizzyy@users.noreply.github.com>
2024-09-27 14:58:57 -0300

    Add files via upload

---
+++ Aluno/src/Principal.java
@@ -0,0 +1,9 @@
+public class Principal {
+    public static void main(String[] args) {
+        Aluno meuAluno = new Aluno();
+        meuAluno.nome = "Isabelle";
+        meuAluno.idade = 18;
+
+        meuAluno.exibeInformacoes();
+    }
+}
